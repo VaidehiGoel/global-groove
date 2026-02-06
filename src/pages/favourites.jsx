9096c119e236faf7d6c4c7fa01a7f3d0724b378e
VaidehiGoel <vaidehigoel21@gmail.com>
2026-02-06 15:40:16 +0530

    add styling to favourites page

--- src/pages/favourites.jsx
+++ src/pages/favourites.jsx
@@ -1,15 +1,41 @@
+import { Link } from "react-router-dom";
+
 function Favorites() {
-  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  const favorites =
+    JSON.parse(localStorage.getItem("favorites")) || [];
 
   return (
-    <div>
-      <h1>Favourites</h1>
+    <div className="favorites-page">
+      <div className="page-content">
+        <h1 className="section-title">Your Favourites</h1>
 
-      {favorites.length === 0 && <p>No favorites yet.</p>}
+        {favorites.length === 0 && (
+          <p className="empty-text">
+            You haven’t added any artists yet.
+          </p>
+        )}
+      </div>
 
-      {favorites.map(a => (
-        <div key={a.id}>{a.name}</div>
-      ))}
+      {favorites.length > 0 && (
+        <div className="favorites-section">
+          <div className="artists-grid">
+            {favorites.map(artist => (
+              <Link
+                to={`/artist/${artist.id}`}
+                key={artist.id}
+                className="artist-card"
+              >
+                <img
+                  src={artist.images[0]?.url}
+                  alt={artist.name}
+                  className="artist-image"
+                />
+                <h4 className="artist-name">{artist.name}</h4>
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
