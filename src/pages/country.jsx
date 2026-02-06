a3103721e9ed2920fb46403c4c2fb11baa6c6023
VaidehiGoel <vaidehigoel21@gmail.com>
2026-02-06 14:46:07 +0530

    display artists names on country page

--- src/pages/country.jsx
+++ src/pages/country.jsx
@@ -1,7 +1,33 @@
+import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getToken, fetchArtistsByCountry } from "../services/spotify";
+
 function Country() {
+  const { code } = useParams();
+  const [artists, setArtists] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadArtists() {
+      const token = await getToken();
+      const data = await fetchArtistsByCountry(token, code);
+      setArtists(data.artists.items);
+      setLoading(false);
+    }
+    loadArtists();
+  }, [code]);
+
+  if (loading) return <p>Loading artists…</p>;
+
   return (
     <div>
-      Country Page
+      <h2>Popular Artists</h2>
+
+      {artists.map(a => (
+        <Link key={a.id} to={`/artist/${a.id}`}>
+          <div>{a.name}</div>
+        </Link>
+      ))}
     </div>
   );
 }
